fix rotation algorithm, redefine path to openpose

main.py
@@ -11,11 +11,11 @@ body_line = [
 ]
 
 if __name__ == "__main__":
-    recg = op_container("../openpose/models/")
-    recg.setImage("./imgs/keqing_left_side.jpg")
+    recg = op_container()
+    recg.setImage("./imgs/keqing_left_side_pose.jpg")
     left = recg.getKeyPoint()
     # cv2.imwrite("./left.jpg", recg.getImage())
-    recg.setImage("./imgs/keqing_right_side.jpg")
+    recg.setImage("./imgs/keqing_right_side_pose.jpg")
     right = recg.getKeyPoint()
     # cv2.imwrite("./right.jpg", recg.getImage())
 
@@ -26,9 +26,11 @@ if __name__ == "__main__":
     out = rebuilder.calc_depth(left_cap, right_cap)
     print(out)
 
-    out = motion_adjust(out, 45)
+    out = motion_adjust(out, -45)
 
     print(out)
+
+    np.save("./3d_motion.npy", out)
 
     line = []
     for i in range(len(body_line)):

rebuild.py
@@ -5,6 +5,7 @@ def motion_adjust(motion : np.ndarray, angle):
     '''params:
        motion: the motion to be adjusted
        angle: the adjust angle in degree, 0 <= theta < 360
+       alert: this function only adjust motion on the X-Y flat
        return:
        the corrected motion
     '''
@@ -12,8 +13,8 @@ def motion_adjust(motion : np.ndarray, angle):
     motion = deepcopy(motion)
     angle = util.degree2radius(util.angle_normalization(angle))
     x, y = deepcopy(motion.T[:2])
-    motion.T[0] = x * np.cos(angle) + y * np.sin(angle)
-    motion.T[1] = x * np.cos(angle) - y * np.sin(angle)
+    motion.T[0] = x * np.cos(angle) - y * np.sin(angle)
+    motion.T[1] = x * np.sin(angle) + y * np.cos(angle)
     return motion
 
 class util: